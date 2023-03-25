fb41d01e0c0e5a6d79a26a3b70d2bcc5af6e6061
Oakmoater <dubrovecva@gmail.com>
2023-03-25 18:38:02 +0100

    almost end

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
 import { fetchCountries } from '../src/js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
@@ -7,12 +8,40 @@ const inputArea = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-inputArea.addEventListener('input', debounce((event) => {
-    fetchCountries(event.currentTarget.value.trim())
-        .then((country) => renderCountry(country[0]))
-        .catch((error) => console.log(error));
+inputArea.addEventListener('input', debounce(event => {
+    fetchCountries(event.target.value.trim())
+      .then(country => {
+        clearPage();
+
+        console.log(country.length);
+
+        if (country.length > 10) {
+          Notify.info('Too many matches found. Please enter a more specific name.');
+        } else if (country.length > 1) {
+          renderCountries(country);
+        } else if (country.length === 1) {
+          renderCountry(country[0]);
+        }
+      })
+      .catch(error => {
+        clearPage();
+        Notify.failure(error.message)
+      });
 }, DEBOUNCE_DELAY));
 
+function renderCountries(countries) {
+  const markup = countries.map(({ flags, name }) => {
+    return `
+    <li class="country-list__item">
+      <div class="country-container">
+        <img class="country-info__flag" src="${flags.svg}" alt="${flags.alt}" />
+        <h2 class="country-info__title">${name.official}</h2>
+      </div>
+    </li>`
+  });
+  countryList.insertAdjacentHTML('beforeend', markup.join(' '));
+  console.log(markup);
+};
 
 function renderCountry({ flags, name, capital, population, languages }) {
   const leng = Object.values(languages).join(', ');
@@ -31,9 +60,13 @@ function renderCountry({ flags, name, capital, population, languages }) {
     </p>
     <p class="country-info__lng">
       <span>Languages: </span>
-        ${leng}
+      ${leng}
     </p>
     `;
-
   countryInfo.insertAdjacentHTML('beforeend', markup);
+};
+
+function clearPage() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
 };
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,10 @@
-export function fetchCountries (name) {
-    return fetch (`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`)
-        .then(response => {
-            if (!response.ok) {
-                if (response.status === 404) {
-                    throw new Error('Oops, there is no country with that name');
-                }
-                throw new Error('Oops, something went wrong!');
-            }
-           return response.json();
+export async function fetchCountries(name) {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`);
+    if (!response.ok) {
+        if (response.status === 404) {
+            throw new Error('Oops, there is no country with that name');
         }
-    );
+        throw new Error('Oops, something went wrong!');
+    }
+    return await response.json();
 };
